381765a42db573c096f8d3d43c666aa0715ceadf
Umakathir <30317782+Umakathir@users.noreply.github.com>
2017-07-29 16:59:42 +0530

    Create gcd.c

---
+++ gcd.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+  int n1,n2,i,gcd;
+  printf("Enter the numbers");
+  scanf("%d%d",&n1,&n2);
+  for(i=0;i<=n1||i<=n2;i++)
+  {
+    gcd=i;
+  }
+  printf("The GCD of %d and %d is %d\n",n1,n2,i)
+}
